d70c152f6d35b77da9d08947a6fc2ee6b58e2d3d
Teodoro B. Mendes <teobmendes@gmail.com>
2022-12-16 23:47:31 -0300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+async function main() {
+    await prisma.user.update({
+        where: {
+            id: 1,
+            name: {
+                not: ""
+            }
+        },
+        data: {
+            name: "Alice"
+        }
+    })
+
+}
+
+main().then(() => console.log("done")).catch(console.error)
